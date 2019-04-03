89b4e85e39f677904f67f7e0dfed74a48301854c
nirbhay <pherwani37@gmail.com>
2019-04-02 20:56:12 -0400

    lat lng support added for entire json

--- __data__/revise.py
+++ __data__/revise.py
@@ -7,13 +7,15 @@ with open("data.json", "r") as f:
   orignalJson = json.loads(f.read())
 
 for counter in range(0, len(orignalJson)):
-    newJson[counter] = orignalJson[counter]
-    g = geocoder.here(newJson[counter]["Nationality"],app_id='DA1Z4YGFPP1CgZfq4mB9',app_code='4FHp0V77LSCZ6On60TsM_g')
-    newJson[counter]["Latitude"] = g.latlng[0]
-    newJson[counter]["Longitude"] = g.latlng[1]
-    print(str(counter) + "----------" + str(newJson[counter]["ID"]))
-    if counter==100:
-        break
+    try:
+        g = geocoder.here(orignalJson[counter]["Nationality"],app_id='DA1Z4YGFPP1CgZfq4mB9',app_code='4FHp0V77LSCZ6On60TsM_g')
+        newJson[counter] = orignalJson[counter]
+        newJson[counter]["Latitude"] = g.latlng[0]
+        newJson[counter]["Longitude"] = g.latlng[1]
+        print(str(counter) + "----------" + str(newJson[counter]["ID"]))
+    except Exception as e:
+        print(e)
+        pass
 
 with open("dataV2.json", "w") as u:
   json.dump(newJson, u)
