6e7cf6b186bbc73aee4d52751e17c8f8baaae26f
nirbhay <pherwani37@gmail.com>
2019-04-05 11:19:46 -0400

    description added to V2. pusing data V3

---
+++ __data__/reviseV3.py
@@ -0,0 +1,24 @@
+import json
+
+v2Json = None
+playerJson = None
+newJson = {}
+with open("dataV2.json", "r") as f:
+  v2Json = json.loads(f.read())
+with open("__crawler__/player_descriptions.json", "r") as f:
+  playerJson = json.loads(f.read())
+
+for counter in range(0, len(v2Json)):
+    try:
+        playerId = v2Json[str(counter)]['ID']
+        description = playerJson[str(playerId)]["description"]
+        newJson[str(counter)] = v2Json[str(counter)]
+        newJson[str(counter)]["description"] = description
+        print(str(counter) + "----------" + str(newJson[str(counter)]["ID"]))
+    except Exception as e:
+        print(str(counter) + "<--COULD NOT FIND DESCRIPTION, DID NOT ADD TO LIST")
+        pass
+
+with open("dataV3.json", "w") as u:
+  json.dump(newJson, u)
+  print("File update complete!")
